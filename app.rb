9f2f9423b1012952b6ef088cd10e6e6431cd3d89
Evan Clough <clough.evan@gmail.com>
2015-08-21 11:24:17 -0700

    Fill in necessary variables in app.rb routing logic.

--- app.rb
+++ app.rb
@@ -19,62 +19,105 @@ get '/'  do
 	erb(:index)
 end
 
-# client routes
-
-get '/clients' do
-
-  erb(:clients)
-end
-
-post '/clients/new' do
-
-  erb(:clients)
-end
-
-get '/clients/:id' do
-  id = params.fetch('id')
-
-  erb(:client)
-end
-
-patch '/clients/:id' do
-  id = params.fetch('id')
-
-  erb(:client)
-end
-
-delete '/clients/:id' do
-  id = params.fetch('id')
-
-  erb(:clients)
-end
-
 # stylist routes
 
 get '/stylists' do
+  @stylists = Stylist.all
 
   erb(:stylists)
 end
 
 post '/stylists/new' do
+  name = params.fetch('name')
+
+  Stylist.new({name: name}).save
+
+  @stylists = Stylist.all
 
   erb(:stylists)
 end
 
 get '/stylists/:id' do
-  id = params.fetch('id')
+  id = params.fetch('id').to_i
+
+  @stylist = Stylist.find(id)
+  @clients = @stylist.clients
 
   erb(:stylist)
 end
 
 patch '/stylists/:id' do
-  id = params.fetch('id')
+  id = params.fetch('id').to_i
+  new_name = params.fetch('new_name')
+
+  @stylist = Stylist.find(id)
+  @stylist.update({name: new_name})
+  @clients = @stylist.clients
 
   erb(:stylist)
 end
 
 delete '/stylists/:id' do
-  id = params.fetch('id')
+  id = params.fetch('id').to_i
+
+  stylist = Stylist.find(id)
+  stylist.delete
+
+  @stylists = Stylist.all
 
   erb(:stylists)
+end
+
+# client routes
+
+get '/clients' do
+  @clients = Client.all
+  @stylists = Stylist.all
+
+  erb(:clients)
+end
+
+post '/clients/new' do
+  name = params.fetch('name')
+  stylist_id = params.fetch('stylist_id')
+
+  client = Client.new({name: name, stylist_id: stylist_id})
+  client.save
+
+  @clients = Client.all
+  @stylists = Stylist.all
+
+  erb(:clients)
+end
+
+get '/clients/:id' do
+  id = params.fetch('id').to_i
+
+  @client = Client.find(id)
+  @stylist = @client.stylist
+
+  erb(:client)
+end
+
+patch '/clients/:id' do
+  id = params.fetch('id').to_i
+  new_name = params.fetch('new_name')
+
+  @client = Client.find(id)
+  @client.update({name: new_name})
+  @stylist = @client.stylist
+
+  erb(:client)
+end
+
+delete '/clients/:id' do
+  id = params.fetch('id').to_i
+
+  client = Client.find(id)
+  client.delete
+
+  @clients = Client.all
+  @stylist = Stylist.all
+
+  erb(:clients)
 end
