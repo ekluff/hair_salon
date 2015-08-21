68d3a1cf1d5111da83ba59ef2253cff224534101
Evan Clough <clough.evan@gmail.com>
2015-08-21 10:42:22 -0700

    Write integration specs for client path.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,64 +4,125 @@ require('sinatra')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('visiting the stylists page', {:type => :feature}) do
-  it 'allows a user to visit a page that displays all the stylists' do
-    visit('/')
-    click_link('stylists')
-    expect(page).to have_content('Stylists')
+describe('the stylist path') do
+  describe('visiting the stylists page', {:type => :feature}) do
+    it 'allows a user to visit a page that displays all the stylists' do
+      stylist = Stylist.new({name: 'Tacocat de Gato'})
+      stylist.save
+      visit('/')
+      click_link('stylists')
+      expect(page).to have_content('Tacocat de Gato')
+    end
+  end
+
+  describe('adding a new stylist', {:type => :feature}) do
+    it 'allows the user to add a new stylist' do
+      visit('/stylists')
+      fill_in('name', with: 'Tacocat de Gato')
+      click_button('+ stylist')
+      expect(page).to have_content('Tacocat de Gato')
+    end
+  end
+
+  describe('visiting a stylist page', {:type => :feature}) do
+    it 'allows you to add click the name of a stylist and see their name and clients' do
+      stylist = Stylist.new({name: 'Tacocat de Gato'})
+      stylist.save
+      client = Client.new({name: 'Tacocat de Perro', stylist_id: stylist.id})
+      client.save
+      visit('/stylists')
+      click_link('Tacocat de Gato')
+      expect(page).to have_content('Tacocat de Gato')
+      expect(page).to have_content('Tacocat de Perro')
+    end
+  end
+
+  describe('adding a new client to on the stylist page') do
+    it 'allows you to add a client to a stylist from the stylist detail page' do
+      stylist = Stylist.new({name: 'Tacocat de Gato'})
+      stylist.save
+      visit("/stylists/#{stylist.id}")
+      fill_in('client_name', with: 'Tacocat de Perro')
+      click_button('+ client')
+      expect(page).to have_content('Tacocat de Perro')
+    end
+  end
+
+  describe('changing the name of a stylist') do
+    it 'allows you to change the name of a stylist' do
+      stylist = Stylist.new({name: 'Tacocat de Gato'})
+      stylist.save
+      visit("/stylists/#{stylist.id}")
+      fill_in('stylist_name', with: 'Tacocat de Perro')
+      click_button('change')
+      expect(page).to have_content('Tacocat de Perro')
+    end
+  end
+
+  describe('deleting a stylist') do
+    it 'allows you to delete a stylist and delivers you to back to the stylists page' do
+      stylist = Stylist.new({name: 'Tacocat de Gato'})
+      stylist.save
+      visit("/stylists/#{stylist.id}")
+      click_button('delete stylist')
+      expect(page).not.to have_content('Tacocat de Gato')
+    end
   end
 end
 
-describe('adding a new stylist', {:type => :feature}) do
-  it 'allows the user to add a new stylist' do
-    visit('/stylists')
-    fill_in('name', with: 'Tacocat de Gato')
-    click_button('+ stylist')
-    expect(page).to have_content('Tacocat de Gato')
+describe('the client path') do
+  describe('visiting the clients page', {:type => :feature}) do
+    it 'allows a user to visit a page that displays all the clients and their stylists' do
+      stylist = Stylist.new({name: 'Tacocat de Gato'})
+      stylist.save
+      client = Client.new({name: 'Tacocat de Perro', stylist_id: stylist.id})
+      client.save
+      visit('/clients')
+      expect(page).to have_content('Tacocat de Perro')
+      expect(page).to have_content('Tacocat de Gato')
+    end
   end
-end
 
-describe('visiting a stylist page', {:type => :feature}) do
-  it 'allows you to add click the name of a stylist and see their name and clients' do
-    stylist = Stylist.new({name: 'Tacocat de Gato'})
-    stylist.save
-    client = Client.new({name: 'Tacocat de Perro', stylist_id: stylist.id})
-    client.save
-    visit('/stylists')
-    click_link('Tacocat de Gato')
-    expect(page).to have_content('Tacocat de Gato')
-    expect(page).to have_content('Tacocat de Perro')
+  describe('adding a new client', {:type => :feature}) do
+    it 'allows the user to add a new client' do
+      visit('/clients')
+      fill_in('client_name', with: 'Tacocat de Gato')
+      click_button('+ client')
+      expect(page).to have_content('Tacocat de Gato')
+    end
   end
-end
 
-describe('adding a new client to on the stylist page') do
-  it 'allows you to add a client to a stylist from the stylist detail page' do
-    stylist = Stylist.new({name: 'Tacocat de Gato'})
-    stylist.save
-    visit("/stylists/#{stylist.id}")
-    fill_in('client_name', with: 'Tacocat de Perro')
-    click_button('+ client')
-    expect(page).to have_content('Tacocat de Perro')
+  describe('visiting a client page', {:type => :feature}) do
+    it 'allows you to add click the name of a client and see their name and stylist' do
+      stylist = Stylist.new({name: 'Tacocat de Gato'})
+      stylist.save
+      client = Client.new({name: 'Tacocat de Perro', stylist_id: stylist.id})
+      client.save
+      visit('/clients')
+      click_link('Tacocat de Gato')
+      expect(page).to have_content('Tacocat de Gato')
+      expect(page).to have_content('Tacocat de Perro')
+    end
   end
-end
 
-describe('changing the name of a stylist') do
-  it 'allows you to change the name of a stylist' do
-    stylist = Stylist.new({name: 'Tacocat de Gato'})
-    stylist.save
-    visit("/stylists/#{stylist.id}")
-    fill_in('stylist_name', with: 'Tacocat de Perro')
-    click_button('change')
-    expect(page).to have_content('Tacocat de Perro')
+  describe('changing the name of a client') do
+    it 'allows you to change the name of a client on the client page' do
+      client = Client.new({name: 'Tacocat de Gato'})
+      client.save
+      visit("/clients/#{client.id}")
+      fill_in('client_name', with: 'Tacocat de Perro')
+      click_button('change')
+      expect(page).to have_content('Tacocat de Perro')
+    end
   end
-end
 
-describe('deleting a stylist') do
-  it 'allows you to delete a stylist and delivers you to back to the stylists page' do
-    stylist = Stylist.new({name: 'Tacocat de Gato'})
-    stylist.save
-    visit("/stylists/#{stylist.id}")
-    click_button('delete stylist')
-    expect(page).not.to have_content('Tacocat de Gato')
+  describe('deleting a client') do
+    it 'allows you to delete a client and delivers you to back to the clients page' do
+      client = Client.new({name: 'Tacocat de Gato'})
+      client.save
+      visit("/clients/#{client.id}")
+      click_button('delete client')
+      expect(page).not.to have_content('Tacocat de Gato')
+    end
   end
 end
